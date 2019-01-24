99fac2343aaa671747cd4328b88cbb2e2028a4db
Christian Esken <chris.svc@arcor.de>
2019-01-24 23:09:19 +0100

    Inform listeners about changes done from quick sorter

--- app/src/main/java/de/todo4you/todo4you/QuickSortActivity.java
+++ app/src/main/java/de/todo4you/todo4you/QuickSortActivity.java
@@ -1,12 +1,11 @@
 package de.todo4you.todo4you;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.time.LocalDate;
 
 import de.todo4you.todo4you.model.Todo;
 import de.todo4you.todo4you.tasks.TaskStore;
@@ -54,12 +53,21 @@ public class QuickSortActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View v) {
         Button button = (Button)v;
+        boolean scheduled = false;
         if (button == quickTodayButton) {
             todo.setDueDate(StandardDates.now());
+            scheduled = true;
         } else if (button == quickTomorrowButton) {
             todo.setDueDate(StandardDates.now().plusDays(1));
+            scheduled = true;
         } if (button == quick1weekButton) {
             todo.setDueDate(StandardDates.now().plusDays(7));
+            scheduled = true;
+        }
+        if (scheduled) {
+            TaskStore.instance().taskModifed(todo);
+            Snackbar.make(v, "Task rescheduled", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
         }
     }
 }
