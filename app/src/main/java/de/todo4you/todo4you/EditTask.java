23be2037d15dd54318f2cef88cef510481dad7dc
Christian Esken <chris.svc@arcor.de>
2020-09-11 00:54:37 +0200

    Add editTask design

---
+++ app/src/main/java/de/todo4you/todo4you/EditTask.java
@@ -0,0 +1,13 @@
+package de.todo4you.todo4you;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_task);
+    }
+}
